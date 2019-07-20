d9cf82a5669866b950d4cd20354744d81195f8b7
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-20 12:52:39 +0100

    Completing and observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,6 +21,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval( () => {
         observer.next(count);
+        if ( count === 2) {
+          observer.complete();
+        }
         if (count > 3) {
           observer.error(new Error('Count is greater than 3!'));
         }
